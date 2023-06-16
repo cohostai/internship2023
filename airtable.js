11b45343fac6c6b05555ef73b8d07024959792c1
phamgialinhlx <phamgialinhlx2@gmail.com>
2023-06-16 14:41:58 +0700

    Refactor code

--- airtable.js
+++ airtable.js
@@ -1,5 +1,5 @@
 // Don't forget to add your API key to the key variable
-const key = ""
+const KEY = ""
 
 let table = base.getTable("Table 1");
 let view = table.getView('Needs Info');
@@ -7,17 +7,14 @@ let result = await view.selectRecordsAsync({fields: ['Prompt']});
 for (let record of result.records) {
     if (record) {
         let prompt = await record.getCellValue('Prompt');
-        output.table(record.getCellValue('Prompt'));
-        output.text(prompt)
         let results = await chatCompletions(prompt)
-        output.text(results)
         await table.updateRecordAsync(record, {[`Social post`]: results,});
     }
 }
 
 async function chatCompletions(prompt) {
     let url = "https://api.openai.com/v1/chat/completions";
-    let temperature = 0.7, maxLeght = 256, topP = 1, frequencyPenalty = 0, presencePenalty = 0;
+    let temperature = 0.7, maxLength = 256, topP = 1, frequencyPenalty = 0, presencePenalty = 0;
 
     // set up the API call variables
     let request = {
@@ -25,7 +22,7 @@ async function chatCompletions(prompt) {
         messages: [{"role": "user", "content": prompt}],
         temperature: temperature,
         top_p: topP,
-        max_tokens: maxLeght,
+        max_tokens: maxLength,
         frequency_penalty: frequencyPenalty,
         presence_penalty: presencePenalty
     }
@@ -33,9 +30,8 @@ async function chatCompletions(prompt) {
     let data = {
         "Content-Type": "application/json",
         "Accept": "application/json",
-        'Authorization': 'Bearer ' + key,
+        'Authorization': 'Bearer ' + KEY,
     };
-    output.text(JSON.stringify(request))
     let response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify(request),
